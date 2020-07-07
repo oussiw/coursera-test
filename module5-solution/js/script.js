9cdfbb226749227bae42efce9c87352f700f8db4
Oussama Siwane <oussamasiwane@student.emi.ac.ma>
2020-07-07 22:41:22 +0100

    commit

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -103,7 +103,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
           // variable's name implies it expects.
           // var chosenCategoryShortName = ....
           var chosenCategoryShortName = chooseRandomCategory(categories);
-          console.log(chosenCategoryShortName)
+          console.log(chosenCategoryShortName.short_name)
           
           // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
           // chosen category from STEP 2. Use existing insertProperty function for that purpose.
