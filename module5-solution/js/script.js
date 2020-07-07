b56d022a2d0447b05a019da2fa3a983a53a7dc66
Oussama Siwane <oussamasiwane@student.emi.ac.ma>
2020-07-07 22:49:12 +0100

    commit

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -102,9 +102,10 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
           // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
           // variable's name implies it expects.
           // var chosenCategoryShortName = ....
-          var chosenCategoryShortName = chooseRandomCategory(categories);
+          var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
           console.log(chosenCategoryShortName.short_name);
           console.log("hello");
+          console.log(homeHtml);
           
           // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
           // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -118,7 +119,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
           // it into the home html snippet.
           //
           // var homeHtmlToInsertIntoMainPage = ....
-
+          var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
 
           // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
           // Use the existing insertHtml function for that purpose. Look through this code for an example
