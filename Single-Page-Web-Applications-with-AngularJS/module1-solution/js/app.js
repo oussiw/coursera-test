6602b1ff311cb70b218b4a2d76f312dac75fc567
oussiw <ous.siw2@gmail.com>
2020-07-11 01:31:32 +0100

    last

--- Single-Page-Web-Applications-with-AngularJS/module1-solution/js/app.js
+++ Single-Page-Web-Applications-with-AngularJS/module1-solution/js/app.js
@@ -1,13 +1,27 @@
 (function () {
     'use strict';
-    angular.module('myFirstApp',[])
-        .controller('myFirstController',DIfunction);
-    DIfunction.$inject = ["$scope", "$filter"];
-    function DIfunction($scope, $filter) {
-        $scope.name = 'Oussama';
-        $scope.upper = function () {
-            var upCase = $filter('uppercase');
-            $scope.name = upCase($scope.name);
+    angular.module('LunchCheck',[])
+        .controller('LunchCheckController',Myfunction);
+    Myfunction.$inject = ["$scope"];
+    function Myfunction($scope) {
+        $scope.listItems = "";
+        $scope.output = "";
+        $scope.checkItems = function () {
+            if($scope.listItems===("")){
+                showMessage("Please enter data first");
+            }
+            else{
+                var list = $scope.listItems.split(",");
+                if(list.length <= 3){
+                    showMessage("Enjoy!");
+                }
+                else{
+                    showMessage("Too much!")
+                }
+            }
+        }
+        function showMessage(string) {
+            $scope.output = string;
         }
     }
 })();
