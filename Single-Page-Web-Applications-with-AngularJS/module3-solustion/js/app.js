e79b93b5728fce338e0494fc24642d00c31b67e2
oussiw <ous.siw2@gmail.com>
2020-09-03 17:47:16 +0100

    Commit

--- Single-Page-Web-Applications-with-AngularJS/module3-solustion/js/app.js
+++ Single-Page-Web-Applications-with-AngularJS/module3-solustion/js/app.js
@@ -1,58 +1,78 @@
-// (function () {
-//     'use strict';
-//     angular.module('ShoppingListCheckOff', [])
-//         .controller('ToBuyController', ToBuyController)
-//         .controller('AlreadyBoughtController', AlreadyBoughtController)
-//         .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
-//
-//
-//     ToBuyController.$inject = ['ShoppingListCheckOffService'];
-//     function ToBuyController(ShoppingListCheckOffService) {
-//         var ctrl = this;
-//
-//         ctrl.listItems = ShoppingListCheckOffService.getToBuyList();
-//
-//         ctrl.markAsBought = function (index) {
-//             ShoppingListCheckOffService.addBoughtItem(index);
-//         }
-//     }
-//
-//     AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-//     function AlreadyBoughtController(ShoppingListCheckOffService) {
-//         var ctrl = this;
-//
-//         ctrl.listItems = ShoppingListCheckOffService.getBoughtList();
-//     }
-//
-//
-//     function ShoppingListCheckOffService() {
-//         var service = this;
-//
-//         var toBuyCart = [
-//             {name: "cookies", quantity: 10},
-//             {name: "sugar pack", quantity: 20},
-//             {name: "beef", quantity: 5},
-//             {name: "sushi", quantity: 2},
-//             {name: "tomato", quantity: 10}
-//         ];
-//
-//         var boughtCart = [];
-//
-//         service.addBoughtItem = function (index) {
-//             var boughtItem = toBuyCart[index];
-//             toBuyCart.splice(index, 1);
-//             boughtCart.push(boughtItem);
-//         };
-//
-//         service.getToBuyList = function () {
-//             return toBuyCart;
-//         }
-//
-//         service.getBoughtList = function () {
-//             return boughtCart;
-//         }
-//     }
-//
-// })();
+(function () {
+    "use strict";
 
-!function(){"use strict";function t(t){this.listItems=t.getToBuyList(),this.markAsBought=function(i){t.addBoughtItem(i)}}function i(t){this.listItems=t.getBoughtList()}angular.module("ShoppingListCheckOff",[]).controller("ToBuyController",t).controller("AlreadyBoughtController",i).service("ShoppingListCheckOffService",function(){var t=[{name:"cookies",quantity:10},{name:"sugar pack",quantity:20},{name:"beef",quantity:5},{name:"sushi",quantity:2},{name:"tomato",quantity:10}],i=[];this.addBoughtItem=function(e){var n=t[e];t.splice(e,1),i.push(n)},this.getToBuyList=function(){return t},this.getBoughtList=function(){return i}}),t.$inject=["ShoppingListCheckOffService"],i.$inject=["ShoppingListCheckOffService"]}();
+    angular.module('module3', [])
+        .controller('MyController', MyController)
+        .service('MyService',MyService)
+        .service('HttpService', HttpService)
+        .constant('ApiBasePath',"https://davids-restaurant.herokuapp.com");
+
+    MyController.$inject = ['MyService','HttpService']
+    function MyController(MyService,HttpService){
+        var ctrl = this;
+
+        ctrl.search = function (){
+            var promise = HttpService.getMenuItems();
+            promise
+                .then(function (response) {
+                    ctrl.menu_items = response.data;
+                })
+                .catch(function (error) {
+                    console.log("There was an error fetching the data!");
+                });
+            ctrl.selected_items = MyService.search(ctrl.searchInput,ctrl.menu_items.menu_items);
+        }
+
+        ctrl.removeItem = function (index){
+            ctrl.selected_items.splice(index,1);
+        }
+    }
+
+    function MyService(){
+        var service = this;
+        service.search = function (search_entry , array){
+            var selectedItems = [];
+            for(var i=0;i<array.length;i++){
+                if(array[i].description.indexOf(search_entry)<0){
+                    console.log("No item found!!");
+                }
+                else{
+                    console.log("Found")
+                    selectedItems.push(array[i]);
+                }
+            }
+            return selectedItems;
+        }
+    }
+
+    HttpService.$inject = ['$http','ApiBasePath']
+    function HttpService($http,ApiBasePath) {
+        var service = this;
+
+        service.getMenuCategories = function () {
+            var response = $http({
+                method: "GET",
+                url: (ApiBasePath+'/categories.json')
+            });
+            return response;
+        }
+
+        service.getMenuByCategory = function (short_name) {
+            var response = $http({
+                method: "GET",
+                url: (ApiBasePath+"/menu_items.json"),
+                params: {category: short_name}
+            })
+            return response;
+        }
+
+        service.getMenuItems = function () {
+            var response = $http({
+                method: "GET",
+                url: (ApiBasePath+"/menu_items.json"),
+            })
+            console.log("Foundccccc")
+            return response;
+        }
+    }
+})();
